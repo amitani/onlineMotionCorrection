00ae5edfad352aafcfca4df30804b4bce8317161
Akinori Mitani <amitani@ucsd.edu>
2016-12-13 13:36:56 -0800

    added RunGuard to limit to one instance

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <RunGuard.h>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    RunGuard guard( "am_motion_correction_runguard" );
+    if (!guard.tryToRun() ){
+        QMessageBox* msgBox = new QMessageBox;
+        msgBox->setWindowTitle("Error");
+        msgBox->setText("Other instance is still running. Please close it first.");
+        msgBox->setIcon(QMessageBox::Critical);
+        msgBox->setWindowFlags(Qt::WindowStaysOnTopHint);
+        msgBox->show();
+        a.exec();
+        return -1;
+    }
     MainWindow w;
     w.setFixedSize(w.size());
     w.show();
